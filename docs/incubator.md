ff7eca6f5818ee4cba194ea44ca4324124b93478
Nguyen Hoang Trung Dung <trungdung@192.168.1.17>
2023-06-01 13:49:40 +0700

    first commit

---
+++ docs/incubator.md
@@ -0,0 +1,30 @@
+---
+sidebar_position: 3
+---
+
+
+# Incubator
+
+## What you'll need
+
+- [Arduino IDE](https://www.arduino.cc/en/software)
+- USB-A to USB-B 2.0
+- Heat Gun or Hot Air Dryer
+- Hot Glue Gun
+
+### 1. Remove the Arduino
+- Open the top cover and remove the device from the lid with a Heat Gun or a Hot Air Dryer.
+- Turn the device upside down.
+- Use a Heat Gun or a Hot Air Dryer to heat up the glue surrounding the fan.
+- Gently remove the fan from its location while leaving the cord connected.
+- Take the Arduino from the green PCB.
+
+
+### 2. Upload firmware to the Arduino
+- From Arduino IDE, open the sketch called **Incubator.ino**
+- Refer to the [**How to upload a sketch**](intro#upload) for instructions.
+
+### 3. Re-assemble the housing
+- Reattach the Arduino (ensure that all pin legs are connected).
+- Glue back the fan in its original position (with the blowing side facing out).
+- Place the device back on the lid and seal the gap with the Hot Glue Gun.
---
+++ docusaurus.config.js
@@ -0,0 +1,76 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  noIndex: true,
+  title: 'Forte Biotech Guide',
+  tagline: '',
+  favicon: 'img/default-favicon.ico',
+
+  url: 'https://tdung939.github.io/', // Your website URL
+  baseUrl: '/fbt_guide/',
+  projectName: 'fbt_guide',
+  organizationName: 'TDung939',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Forte Biotech Guide',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/fbt_logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'How to',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Forte Biotech.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
